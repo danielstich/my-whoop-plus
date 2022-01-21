a43929e8d64cff24f49745d913acee59d2583759
Ka-Kit Jimmy Tam <90652743+jimmykt@users.noreply.github.com>
2022-01-21 11:37:38 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.scss';
-import home from './assets/home_screen.jpeg';
-import OccupationSearch from './components/OccupationSearch/OccupationSearch';
+import "./App.scss";
+import home from "./assets/home_screen.jpeg";
+import OccupationSearch from "./components/OccupationSearch/OccupationSearch";
+import Water from "./components/DrinkWater/DrinkWater";
 
 function App() {
   return (
     <div className="App">
-      <img className='App__img' src={home} alt="" />
+      <img className="App__img" src={home} alt="" />
       <div className="App__container">
-        <OccupationSearch />
+        <Water />
+        {/* <OccupationSearch /> */}
       </div>
     </div>
   );
---
+++ src/components/DrinkWater/DrinkWater.js
@@ -0,0 +1,11 @@
+import "./DrinkWater.scss";
+
+function DrinkWater() {
+  return (
+    <div className="water">
+      <h1 className="water__title">DRINK MORE WATER</h1>
+    </div>
+  );
+}
+
+export default DrinkWater;
