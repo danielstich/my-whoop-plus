6bbe2da935c0f18555cee36dd69c0ac7fdf3b37d
Ka-Kit Jimmy Tam <90652743+jimmykt@users.noreply.github.com>
2022-01-21 12:49:23 -0500

    water component

--- src/components/DrinkWater/DrinkWater.js
+++ src/components/DrinkWater/DrinkWater.js
@@ -1,10 +1,23 @@
 import "./DrinkWater.scss";
 
 function DrinkWater() {
+  const clickHandler = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className="water">
-      <h1 className="water__title">DRINK MORE WATER</h1>
-    </div>
+    <form className="water">
+      <h2 className="water__title">DRINK MORE WATER</h2>
+      <p className="water__info">
+        IN THE PAST MONTH, YOUR AVERAGE WATER INTAKE IS 1.5L PER DAY, WE SUGGEST
+        EVERY ADULT SHOULD TAKE ATLEAST 2L WATER A DAY.
+      </p>
+
+      <input type="range" min="1" max="100" value="50" />
+      <p className="water__metric">2L</p>
+      <button className="water__button" onClick={clickHandler}>
+        CONFIRM
+      </button>
+    </form>
   );
 }
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import OccupationSearch from '../../components/OccupationSearch/OccupationSearch';
-import Notifications from '../../pages/Notifications/Notifications';
-import home from '../../assets/home_screen.jpeg';
-import './Main.scss'
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import OccupationSearch from "../../components/OccupationSearch/OccupationSearch";
+import Notifications from "../../pages/Notifications/Notifications";
+import WaterIntake from "../../components/DrinkWater/DrinkWater";
+import home from "../../assets/home_screen.jpeg";
+import "./Main.scss";
 
 export default class Main extends Component {
-    state = {
-        loadSearch: false
-    }
-    componentDidMount() {
-        setTimeout( ()=> {
-            this.setState({loadSearch: true})
-        }, 2000)
-    }
-    render() {
-        return (
-            <div className="Main">
-                <img className='Main__img' src={home} alt="" />
-                <div className="Main__container">
-                    {this.state.loadSearch && <OccupationSearch />}
-                    {/* <Notifications /> */}
-                </div>
-            </div>
-        );
+  state = {
+    loadSearch: false,
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loadSearch: true });
+    }, 2000);
+  }
+  render() {
+    return (
+      <div className="Main">
+        <img className="Main__img" src={home} alt="" />
+        <div className="Main__container">
+          {/* {this.state.loadSearch && <OccupationSearch />} */}
+          {this.state.loadSearch && <WaterIntake />}
+
+          {/* <Notifications /> */}
+        </div>
+      </div>
+    );
   }
 }
