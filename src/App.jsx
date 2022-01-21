8ce8ea8f92d4964d72eb4f646ac5f85834a2220e
danielstich <43919186+danielstich@users.noreply.github.com>
2022-01-21 11:15:11 -0500

    added start screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Main from './pages/Main/Main';
+import Start1 from './pages/Start-1/Start1';
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
       <Router>
         <Switch>
           <Redirect exact from='/' to='/Main'/>
-          {/* <Route path='/Start-1' component={Start-1} />
-          <Route path='/Start-2' component={Start-2} /> */}
+          <Route path='/Start-1' component={Start1} />
+          {/* <Route path='/Start-2' component={Start-2} /> */}
           <Route path='/Main' exact component={Main} />
         </Switch>
       </Router>
---
+++ src/pages/Start-1/Start1.jsx
@@ -0,0 +1,9 @@
+import img from '../../assets/page1.png'
+import './Start1.scss'
+
+export default function Start1() {
+    return (
+        <div className='start1'>
+            <img className='start1__img' src={img} alt="" />
+        </div>);
+}
