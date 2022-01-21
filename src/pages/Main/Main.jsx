9b51faa2e7a23763e71377b00b67ec227cea1ff7
danielstich <43919186+danielstich@users.noreply.github.com>
2022-01-21 11:05:41 -0500

    added notification header component and routes

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import OccupationSearch from '../../components/OccupationSearch/OccupationSearch';
+import Notifications from '../../pages/Notifications/Notifications';
+import home from '../../assets/home_screen.jpeg';
+import './Main.scss'
+
+export default class Main extends Component {
+    state = {
+        loadSearch: false
+    }
+    componentDidMount() {
+        setTimeout( ()=> {
+            this.setState({loadSearch: true})
+        }, 2000)
+    }
+    render() {
+        return (
+            <div className="Main">
+                <img className='Main__img' src={home} alt="" />
+                <div className="Main__container">
+                    {/* {this.state.loadSearch && <OccupationSearch />} */}
+                    <Notifications />
+                </div>
+            </div>
+        );
+  }
+}
